83aa1a5115bea6d499d0e140b549f0bf96b21b3c
Prabin Acharya <acharyaprabin101@gmail.com>
2022-12-07 19:48:06 +0545

    customize circle and rectangel in DL

--- components/Map.tsx
+++ components/Map.tsx
@@ -32,6 +32,23 @@ const Map = () => {
           markerOptions: {
             icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
           },
+          circleOptions: {
+            fillColor: "#ffff00",
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+          },
+
+          rectangleOptions: {
+            fillColor: "#ffff00",
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+          },
         }}
       />
 
